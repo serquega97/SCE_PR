e37ec43612a8c30f44c111b7a82d5dc4742de953
Sergi Quevedo Garreta <sergiquevedogarreta@MacBook-Air-de-Sergi.local>
2020-04-22 14:20:34 +0200

    sergi:controller season funciona

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'welcome', to: 'pages#home'
   get 'products', to: 'products#get_products'
   get 'products/:season', to: 'products#get_products_season', as: 'products_s'
-  get 'products/:type', to: 'products#get_products_type', as: 'products_t'
+  get 'products/:productType', to: 'products#get_products_type', as: 'products_t'
 end
