5371a85a09e869e066048d7e6c20da7a84dad297
Sergi Quevedo Garreta <sergiquevedogarreta@MacBook-Air-de-Sergi.local>
2020-05-03 11:46:17 +0200

    sergi: añadida página de sign in y de sign up del custoomer

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root 'pages#home'
   get 'welcome', to: 'pages#home'
-  #get 'products', to: 'products#get_products'
-
+  get 'signin', to: 'pages#signin'
+  get 'signup', to: 'pages#signup'
+  post 'new_user', to: 'pages#register'
   get 'products', to: 'products#get_products'
-
-
 end
