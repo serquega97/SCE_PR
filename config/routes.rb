687632836454ca6480cb22fc904a278caa9766b1
Sergi Quevedo Garreta <sergiquevedogarreta@MacBook-Air-de-Sergi.local>
2020-05-03 16:13:33 +0200

    sergi:errores git solucionados

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'welcome', to: 'pages#home'
-<<<<<<< HEAD
-=======
   get 'signin', to: 'pages#signin'
   get 'signup', to: 'pages#signup'
   post 'new_user', to: 'pages#register'
->>>>>>> tmp
   get 'products', to: 'products#get_products'
 end
